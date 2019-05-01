06abcb17888d7ce231e133b9acc388b66327f2b8
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2019-05-01 12:20:06 +0100

    script

--- preproc/Preproc.R
+++ preproc/Preproc.R
@@ -3,19 +3,47 @@
 
 # Data pre-processing:
 
+
+
+# Calvin 1-21
+# Victoria- 22- 43
+# Martin 44+
+
 library(EMreading)
 
 
-# COmprehension accuracy:
+# Comprehension accuracy:
 
-Quest<- Question(data_list = "D:/Data/Font_size/new", maxtrial = 100)
+Quest<- Question(data_list = "D:/Data/Font_size", maxtrial = 100)
 
 save(Quest, file= "data/Quest.Rda")
 
 
+library(reshape)
+DesQuest<- melt(Quest, id=c('sub', 'item', 'cond'), 
+                measure=c("accuracy"), na.rm=TRUE)
+mQuest<- cast(DesQuest, item ~ variable
+              ,function(x) c(M=signif(mean(x),3)
+                             , SD= sd(x) ))
+
+
 # Trial times:
-Trialt<- trialTime(data_list = "D:/Data/Font_size/new", maxtrial = 100)
+Trialt<- trialTime(data_list = "D:/Data/Font_size/", maxtrial = 100)
 
 save(Trialt, file= "data/Trial_time.Rda")
 
+
+DesTime<- melt(Trialt, id=c('sub', 'item', 'cond'), 
+                measure=c("duration_ms"), na.rm=TRUE)
+mTime<- cast(DesTime, cond ~ variable
+              ,function(x) c(M=signif(mean(x),3)
+                             , SD= sd(x) ))
+
+
+
 M<- MultiLine(data_list = "D:/Data/Font_size/new", maxtrial = 100, reAlign = F)
+
+
+EyeDoctor_PadLines("D:/Data/Font_size/")
+
+
