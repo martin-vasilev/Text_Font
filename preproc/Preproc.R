cc9b20b0cf4c4e82a462dae0953b0b89ae576784
Victoria Adedeji <vadedeji@bournemouth.ac.uk>
2019-05-07 14:37:07 +0100

    Files 22 to 25 preprocessed

--- preproc/Preproc.R
+++ preproc/Preproc.R
@@ -5,17 +5,22 @@
 
 library(EMreading)
 
+#hgfdgdfcf
 
 # COmprehension accuracy:
 
-Quest<- Question(data_list = "D:/Data/Font_size/new", maxtrial = 100)
+Quest<- Question(data_list = "E:/FontSizeData", maxtrial = 100)
 
 save(Quest, file= "data/Quest.Rda")
 
 
 # Trial times:
-Trialt<- trialTime(data_list = "D:/Data/Font_size/new", maxtrial = 100)
+Trialt<- trialTime(data_list = "E:/FontSizeData", maxtrial = 100)
 
 save(Trialt, file= "data/Trial_time.Rda")
 
 M<- MultiLine(data_list = "D:/Data/Font_size/new", maxtrial = 100, reAlign = F)
+
+data_dir<-setwd()
+
+Paddedfiles <-EyeDoctor_PadLines(data_dir = "E:/FontSizeData", paddingSize = 5)
