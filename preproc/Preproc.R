bdf0bde035d60976165aca3c62657769645c6f87
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2019-05-15 14:18:44 +0100

    updated preproc script

--- preproc/Preproc.R
+++ preproc/Preproc.R
@@ -1,40 +1,47 @@
 
-# MArtin R. Vasilev, 2019
+# Martin R. Vasilev, 2019
 
 # Data pre-processing:
 
 
-#hgfdgdfcf
+# Manual pre-processing of asc files:
 
 # Calvin 1-21
 # Victoria- 22- 43
 # Martin 44+
 
-library(EMreading)
+#Paddedfiles <-EyeDoctor_PadLines(data_dir = data_dir, paddingSize = 5)
 
-#data_dir= "D:/Data/Font_size" # Martin
-data_dir= "E:/FontSizeData" # Victoria
+# Install/ load R package used in preprocessing:
+
+if('EMreading' %in% rownames(installed.packages())==FALSE){
+  if('devtools' %in% rownames(installed.packages())==FALSE){
+    install.packages('devtools')
+    library(devtools)
+  }else{
+    library(devtools)
+  }
+  install_github('martin-vasilev/EMreading')
+}else{
+  library(EMreading)
+}
+
+
+data_dir= "D:/Data/Font_size" # Martin
+#data_dir= "E:/FontSizeData" # Victoria
 
 ###########################
 # Comprehension accuracy: #
 ###########################
 
-Quest<- Question(data_list = data_dir, maxtrial = 100)
+if(!file.exists("data/Quest.Rda")){
+  Quest<- Question(data_list = data_dir, maxtrial = 100)
+  save(Quest, file= "data/Quest.Rda")
+  write.csv2(Quest, "data/Quest.csv")
+} else{
+  load("data/Quest.Rda")
+}
 
-
-save(Quest, file= "data/Quest.Rda")
-
-
-# Trial times:
-Trialt<- trialTime(data_list = data_dir, maxtrial = 100)
-
-save(Trialt, file= "data/Trial_time.Rda")
-
-M<- MultiLine(data_list = data_dir, maxtrial = 100, reAlign = F)
-
-#data_dir<-setwd()
-
-Paddedfiles <-EyeDoctor_PadLines(data_dir = data_dir, paddingSize = 5)
 
 library(reshape)
 DesQuest<- melt(Quest, id=c('sub', 'item', 'cond'), 
@@ -42,26 +49,42 @@ DesQuest<- melt(Quest, id=c('sub', 'item', 'cond'),
 mQuest<- cast(DesQuest, sub ~ variable
               ,function(x) c(M=signif(mean(x),3)
                              , SD= sd(x) ))
+mQuest
 
 ################
 # Trial times: #
 ################
 
-Trialt<- trialTime(data_list = data_dir, maxtrial = 100)
-
-save(Trialt, file= "data/Trial_time.Rda")
-
+if(!file.exists("data/Trial_time.Rda")){
+  
+  Trialt<- trialTime(data_list = data_dir, maxtrial = 100)
+  save(Trialt, file= "data/Trial_time.Rda")
+  write.csv2(Trialt, "data/Trial_time.csv")
+}else{
+  load("data/Trial_time.Rda")
+}
 
 DesTime<- melt(Trialt, id=c('sub', 'item', 'cond'), 
                 measure=c("duration_ms"), na.rm=TRUE)
 mTime<- cast(DesTime, cond ~ variable
               ,function(x) c(M=signif(mean(x),3)
                              , SD= sd(x) ))
+mTime
+
+
+##################
+# Raw Fixations: #
+##################
+
+if(!file.exists("preproc/raw_fix.Rda")){
+  # extract raw data & merge it with da1 files:
+  raw_fix<- preprocFromDA1(data_dir = data_dir, maxtrial = 100, padding = 5)
+  save(raw_fix, file= "preproc/raw_fix.Rda")
+}else{
+  load("preproc/raw_fix.Rda")
+}
 
 
 
-#M<- MultiLine(data_list = "D:/Data/Font_size/new", maxtrial = 100, reAlign = F)
 
-
-EyeDoctor_PadLines(data_dir)
 
