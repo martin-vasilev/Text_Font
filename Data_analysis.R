693095e77261bf2d88427204b524af24d1965789
Victoria Adedeji <vadedeji@bournemouth.ac.uk>
2019-05-28 13:20:12 +0100

    Updated data analysis script (Calvin & Victoria)

--- Data_analysis.R
+++ Data_analysis.R
@@ -49,7 +49,7 @@ rm(list= ls())
 
 
 # load/ install required packages:
-packages= c("reshape", "lme4", "ggplot2") # list of used packages:
+packages= c("reshape", "lme4", "ggplot2", "MASS", "arm", "effects", "lattice") # list of used packages:
 
 for(i in 1:length(packages)){
   
@@ -66,3 +66,83 @@ for(i in 1:length(packages)){
 load("data/Alldata.Rda")
 load("data/Return_sweep.Rda")
 
+#classify data
+RS$sub = as.factor(RS$sub)
+RS$item = as.factor(RS$item)
+RS$cond= as.factor(RS$cond)
+RS$land_pos = as.numeric(RS$land_pos)
+
+#examine data 
+str(RS)
+head(RS)
+
+#merge conditions for main effect analysis 
+
+RS$line_len= factor(ifelse(RS$cond==1| RS$cond==2, 1,2),labels = c("short line", "long line"))
+RS$font_size= factor(ifelse(RS$cond==1| RS$cond==3, 1,2), labels = c("small font", "big font"))
+
+
+#mean under_sweep per condition 
+USP<- melt(RS, id=c('sub', 'item', 'font_size', 'line_len'), 
+                measure=c("undersweep_prob"), na.rm=TRUE)
+mUSP<- cast(USP, font_size + line_len ~ variable
+              ,function(x) c(M=signif(mean(x),3)
+                             , SD= sd(x) ))
+
+#mean land_pos in letters per condition 
+LP<- melt(RS, id=c('sub', 'item', 'font_size', 'line_len'), 
+           measure=c("LandStartLet"), na.rm=TRUE)
+mLP<- cast(LP, font_size + line_len ~ variable
+            ,function(x) c(M=signif(mean(x),3)
+                           , SD= sd(x) ))
+
+#mean land_pos in visual angle per condition 
+LP1<- melt(RS, id=c('sub', 'item', 'font_size', 'line_len'), 
+          measure=c("LandStartVA"), na.rm=TRUE)
+mLP1<- cast(LP1, font_size + line_len ~ variable
+           ,function(x) c(M=signif(mean(x),3)
+                          , SD= sd(x) ))
+#plots for undersweep probability per condtion 
+tapply(RS$undersweep_prob, list(RS$sub,RS$cond), FUN=mean)
+colnames(mUPPS)= c("SFSL", "SFLL", "BFSL", "BFLL")
+boxplot(mUPPS)
+
+#plots for land_pos per condition for letters
+boxplot(RS$LandStartLet~RS$cond)
+
+ggplot(RS, aes(x=as.factor(RS$cond), y=RS$LandStartLet)) + 
+  geom_boxplot(fill="slateblue", alpha=0.2) +
+  xlab("Conditions") + ylim(0,15)
+  
+#plots for land_pos per condition for visual angle
+
+boxplot(RS$LandStartVA~RS$cond)
+
+ggplot(RS, aes(x=as.factor(RS$cond), y=RS$LandStartVA)) + 
+  geom_boxplot(fill="slateblue", alpha=0.2) +
+  xlab("Conditions") + ylim(0,4)
+
+
+#########GLMM for undersweep probability
+#setting contrast
+contrasts(RS$line_len) <- contr.sdif(2)  
+contrasts(RS$font_size) <- contr.sdif(2)
+
+GLM1=glmer(undersweep_prob ~ font_size + line_len
+           +launchDistVA +(1|item)+(1|sub) , data= RS, family= binomial)
+summary(GLM1)
+plot(allEffects(GLM1))
+
+####LMM for landing position
+land_pos.lm= lmer(LandStartLet ~ line_len *font_size*launchDistLet + 
+                   (1+ line_len+ font_size|sub), RS, REML=T)
+summary(land_pos.lm)
+plot(allEffects(land_pos.lm))
+
+#and
+
+land_pos.lm2= lmer(LandStartVA ~ line_len *font_size*launchDistVA + 
+                      (1|item) + (1+ font_size|sub), RS, REML=T)
+summary(land_pos.lm2)
+plot(allEffects(land_pos.lm2))
+     
